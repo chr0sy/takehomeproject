352e54373489012744e342a0cd5bfa5d93567be9
Ted Reka <rekated97@gmail.com>
2022-10-26 20:18:18 +0200

    Updates

--- src/BlogPost.js
+++ src/BlogPost.js
@@ -1,67 +1,73 @@
-import './style.scss';
-import {Row} from '@canonical/react-components'
-import {Col} from '@canonical/react-components'
-import {Card} from '@canonical/react-components'
-import React, {Component} from 'react';
-import Moment from 'moment';
-
+import "./style.scss";
+import { Row } from "@canonical/react-components";
+import { Col } from "@canonical/react-components";
+import { Card } from "@canonical/react-components";
+import React, { Component } from "react";
+import Moment from "moment";
 
 export default class BlogPost extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      data: []
+      data: [],
     };
   }
 
   componentDidMount() {
     fetch("https://people.canonical.com/~anthonydillon/wp-json/wp/v2/posts")
-      .then(results => results.json())
-      .then(results => {
+      .then((results) => results.json())
+      .then((results) => {
         // console.log(results);
         this.setState({ data: results });
       });
   }
 
-  
-
   render() {
     return (
       <div className="blog-posts">
-      <Row>
-          {this.state.data.map(function(item, index)
-           {
+        <Row>
+          {this.state.data.map(function (item, index) {
             return (
-                  <Col size={4}>
-                    <Card className="canonicalCard">
-                    <div className="topText">
-                    {item._embedded['wp:term'][1].map((name, idx) => {
+              <Col size={4}>
+                <Card className="canonicalCard">
+                  <div className="topText">
+                    {item._embedded["wp:term"][1].map((name, idx) => {
                       return (
-                        <span className="categories" key={idx}>{ (idx ? ', ' : '') + name.name }</span>
+                        <span className="categories" key={idx}>
+                          {(idx ? ", " : "") + name.name}
+                        </span>
                       );
                     })}
-                    </div>
-                    <img key={item.index} src={item.featured_media} />
-                    <a href={item.link}><h3 key={item.index} className="postTitle">{item.title.rendered}</h3></a>
-                    <p className="postMeta">
+                  </div>
+                  <img key={item.index} src={item.featured_media} />
+                  <a href={item.link}>
+                    <h3 key={item.index} className="postTitle">
+                      {item.title.rendered}
+                    </h3>
+                  </a>
+                  <p className="postMeta">
                     {item._embedded.author.map((author, idx) => {
                       return (
-                        <p key={idx}>By <a href={author.link}>{author.name}</a> on {Moment(item.date).format("Do MMMM YYYY")}</p>
+                        <p key={idx}>
+                          By <a href={author.link}>{author.name}</a> on{" "}
+                          {Moment(item.date).format("Do MMMM YYYY")}
+                        </p>
                       );
                     })}
-                      </p>
-                    
-                    <div className="bottomText">
-                    {item._embedded['wp:term'][0].map((name, idx) => {
+                  </p>
+
+                  <div className="bottomText">
+                    {item._embedded["wp:term"][0].map((name, idx) => {
                       return (
-                        <p className="categories" key={idx}>{name.name}</p>
+                        <p className="categories" key={idx}>
+                          {name.name}
+                        </p>
                       );
                     })}
-                    </div>
-                    </Card>
-                  </Col>
-                
+                  </div>
+                </Card>
+              </Col>
             );
           })}
         </Row>
@@ -70,6 +76,4 @@ export default class BlogPost extends Component {
   }
 }
 
-
-
-// 
+//
